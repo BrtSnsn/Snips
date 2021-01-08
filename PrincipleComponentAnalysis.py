8a3d6ef97387472ed47963fef8e170c5e353fd5a
Bert Sansen <bert.sansen@gmail.com>
2021-01-08 12:13:08 +0100

    Feedback Kevin Dunn: stdev calculation using ddof = 1
    Signed-off-by: Bert Sansen <bert.sansen@gmail.com>

--- PrincipleComponentAnalysis.py
+++ PrincipleComponentAnalysis.py
@@ -5,29 +5,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 # from pca import pca  # https://github.com/erdogant/pca
 import seaborn as sns
+from scipy.stats import zscore
 sns.set()
 """https://stackoverflow.com/questions/46732075/python-pca-plot-using-hotellings-t2-for-a-confidence-interval/63043840#63043840"""
 
 
 file = r"C:\Users\bsa\OneDrive - Orac Holding\Desktop\DOE\food-texture.csv"
 data = pd.read_csv(file, index_col=[0], decimal='.', sep=',')
-# print(data)
-# plt.plot(data)
 
 '''by the book nu, scalen, fitten, transformen?'''
-x = StandardScaler().fit_transform(data)
-print(pd.DataFrame(x))
-# x = data.values
+# Dit is hoe ik het deed // Do not use
+# x = StandardScaler().fit_transform(data)  # np.std() gebruikt ddof=0
+# print(f"The centering vector (mean) is: {preproc.mean_}")
+# print(f"The scaling vector (stdev) is: {preproc.scale_}")
+# print(pd.DataFrame(preproc))
+
+# # > Kevin Dunn 06/01/21
+# preproc = StandardScaler().fit(data)
+# print(f"The centering vector (mean) is: {preproc.mean_}")
+# print(f"The scaling vector (stdev) is: {preproc.scale_}")
+# mcuv_X = preproc.transform(data)
+
+# scaling & fitting @ ddof=1
+# https://stackoverflow.com/questions/49150309/why-does-the-standardization-differ-between-pythons-standardscaler-and-matlab
+print('Mean', np.mean(data, axis=0))
+print('Stdev ddof=1\n', np.std(data, ddof=1, axis=0))
+mcuv_X = zscore(data, ddof=1)
+
 plt.figure()
-plt.boxplot(x)  # ziet er ok uit
+plt.boxplot(mcuv_X)  # ziet er ok uit
 plt.gca().set(title='Scaled values')
-model = PCA(svd_solver='full').fit(x)
+model = PCA(svd_solver='full').fit(mcuv_X)
 
 plt.figure()
 plt.plot(np.cumsum(model.explained_variance_ratio_), color='red')
 plt.bar(np.arange(model.n_components_), model.explained_variance_ratio_)
 plt.gca().set(title='95%')
-print('components waarmee t-waarde wordt berekend:\n', pd.DataFrame(model.components_.T, index=data.columns))
+# print('components waarmee t-waarde wordt berekend:\n', pd.DataFrame(model.components_.T, index=data.columns))
 
 plt.figure()
 plt.subplot(211)
@@ -39,7 +53,7 @@ plt.gca().set(title='PC2', ylabel='second component loading')
 plt.tight_layout()
 
 plt.figure()
-plotpca = model.transform(x)
+plotpca = model.transform(mcuv_X)
 plt.scatter(x=plotpca[:, 0], y=plotpca[:, 1])
 for i, txt in enumerate(data.index):
     plt.annotate(i, (plotpca[:, 0][i], plotpca[:, 1][i]))
@@ -47,7 +61,7 @@ plt.gca().set(title='scores', xlabel='first component', ylabel='second component
 
 plt.figure()
 plt.plot(plotpca[:, 0])
-print(plotpca[:, 0][32])
+# print(plotpca[:, 0][32])
 plt.gca().set(title='scores van PC1')
 
 plt.figure()
@@ -57,8 +71,8 @@ for i, txt in enumerate(data.columns):
 plt.gca().set(xlabel='first component', ylabel='second component')
 plt.show()
 
-print(data.reset_index())
-print(data.mean())
+# print(data.reset_index())
+# print(data.mean())
 
 
 
